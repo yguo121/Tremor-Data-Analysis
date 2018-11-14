88a0c832d96b1a498b46a2e817f23915247bcea4
Shilin Li <sl4261@columbia.edu>
2018-11-14 02:57:38 -0500

    Update restructure

--- lib/restructure_dataset.R
+++ lib/restructure_dataset.R
@@ -74,7 +74,7 @@ train_secondhand$label <- 1
 
 ### Combine the firsthand and secondhand
 dt_train <- abind(train_firsthand[c(-1:-30),],train_secondhand[c(-1:-30),],along = 1)
-save(dt_train, file="../data/train_set/train.RData")
+#save(dt_train, file="../data/train_set/train.RData")
 
 
 ########################################
@@ -82,27 +82,6 @@ save(dt_train, file="../data/train_set/train.RData")
 ########################################
 
 dt_test <- abind(train_firsthand[308:337,],train_secondhand[308:337,],along = 1)
-save(dt_test, file="../data/test_set/test.RData")
+#save(dt_test, file="../data/test_set/test.RData")
 
 
-
-
-dataframe_patient$timestamp <- seq(0,3600,length.out = length(dataframe_patient$timestamp))
-fft_data <- fft(dataframe_patient$rotationRate_x)[1:as.integer(nrow(dataframe_patient)/2)]
-fft_data <- mapply(abs,fft_data)
-x <- seq(0, 15, length.out = as.integer(nrow(dataframe_patient)/2))
-fft_data <- data.frame(x,fft_data)
-sample_rate <- get_hertz(dataframe_patient)
-# Plot the fft as a whole to get the frequency
-
-# Transform the data by obtaining the frequency and amplitude and 10s/20s/.../120s interval
-
-dataframe_patient_new <- dataframe_patient[7:15]
-n_time <- seq(10,120,by = 10)
-fft <- get_fft_freq_amp(dataframe_patient_new[,1],n_time,sample_rate)
-total_fft<-fft[1]
-for(i in 1:ncol(dataframe_patient_new)){
-  fft <- get_fft_freq_amp(dataframe_patient_new[,i],n_time,sample_rate)
-  colnames(fft) <- paste0(colnames(dataframe_patient_new)[i],'.',colnames(fft))
-  total_fft <- cbind(total_fft,fft[-1])
-}
