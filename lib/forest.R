8450e4fdf8e2c5a7d65f57a2aba3d8747c9626cd
Shilin Li <sl4261@columbia.edu>
2018-11-26 16:26:11 -0500

    Update forest / svm

---
+++ lib/forest.R
@@ -0,0 +1,44 @@
+########################################
+############# Random Forest ########
+########################################
+library(caret)
+
+library(MASS)
+library(randomForest)
+
+load("../data/train_set/train.RData")
+dt_train <- as.data.frame(dt_train)
+dt_train$label <- as.factor(dt_train$label)
+
+load("../data/test_set/test.RData")
+dt_test <- as.data.frame(dt_test)
+dt_test$label <- as.factor(dt_test$label)
+
+# Normalize the Data
+for (i in 1:18) {
+  dt_train[,i] <- (dt_train[,i] - mean(dt_train[,i]))/sd(dt_train[,i])
+  dt_test[,i] <- (dt_test[,i] - mean(dt_test[,i]))/sd(dt_test[,i])
+}
+
+
+## Train
+forest_fit <- randomForest(label ~ ., data = dt_train, ntree=100, mtry = 5)
+#forest_fit <- randomForest(label ~ ampGravity_z + freqRotation_x + ampGravity_y + ampAcceleration_z + ampGravity_x + ampRotation_x + ampAcceleration_x + ampAcceleration_y + ampRotation_z + freqAcceleration_y, data = dt_train, ntree = 100, mtry = 5)
+
+
+plot(forest_fit)
+forest_fit
+
+### Rank Variable Importance
+varImpPlot(forest_fit,  
+           sort = T,
+           n.var=18,
+           main="Top 18 - Variable Importance")
+
+
+###Test
+
+forest_pred <- predict(forest_fit,newdata = dt_test)
+table(forest_pred,dt_test$label)
+confusionMatrix(data = forest_pred, reference = dt_test$label)
+
--- lib/restructure_dataset.R
+++ lib/restructure_dataset.R
@@ -47,16 +47,17 @@ train_firsthand$label <- 0
 
 # load 11 Secondhand csv and combine to one dataframe
 dataFiles2 <- lapply(Sys.glob("2018071*.csv"), read.csv)
+hert <- get_hertz(dataFiles2[[1]][1:(120*33),])
 dt_bind2 <- dataFiles2[[1]]
 for (i in 2:12) {
-  dt_bind2 <- abind(dt_bind2,dataFiles[[i]],along = 1)
+  dt_bind2 <- abind(dt_bind2,dataFiles2[[i]],along = 1)
 }
 
 # extract 9 varables we interested in
 dt_new2 <- dt_bind2[,7:15]
-mat2 <- array(NA,dim = c(338,18))
+mat2 <- array(NA,dim = c(231,18))
 
-for (i in 0:337) {
+for (i in 0:230) {
   dt2 = dt_new2[(120*33*i+1):(120*33*(i+1)),]
   for (j in 1:9) {
     fft = get_fft_freq_amp(dt2[,j],1,hert)
@@ -64,7 +65,7 @@ for (i in 0:337) {
   }
 }
 # remove the row with -Inf
-mat2 <- mat2[-246,]
+mat2 <- mat2[-46,]
 
 # transfer mat2 to dataframe
 train_secondhand <- as.data.frame(mat2)
@@ -81,7 +82,7 @@ dt_train <- abind(train_firsthand[c(-1:-30),],train_secondhand[c(-1:-30),],along
 ##### Construct Test Set ########
 ########################################
 
-dt_test <- abind(train_firsthand[308:337,],train_secondhand[308:337,],along = 1)
+dt_test <- abind(train_firsthand[1:30,],train_secondhand[1:30,],along = 1)
 #save(dt_test, file="../data/test_set/test.RData")
 
 
